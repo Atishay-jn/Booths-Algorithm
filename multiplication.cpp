23a9e3ec3a268a7113e65883392f718030863394
atishayay <72191705+atishayay@users.noreply.github.com>
2020-10-01 13:35:02 +0530

    Update multiplication.cpp

--- multiplication.cpp
+++ multiplication.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define endl "\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define fast_io() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 using namespace std;
 
 vector<bool> comp(vector<bool> v) {
